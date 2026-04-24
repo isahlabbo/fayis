85bbac98923ec387a1a88c091b3bedd9f6fb1082
labboisah <isahlabbo77@gmail.com>
2026-04-24 09:23:11 +0100

    fixed

--- app/Models/SectionClassStudent.php
+++ app/Models/SectionClassStudent.php
@@ -205,7 +205,7 @@ class SectionClassStudent extends BaseModel
     {
         $term = null;
         foreach($this->sectionClassStudentTerms as $sectionClassStudentTerm){
-            if(strtotime($sectionClassStudentTerm->academicSessionTerm->start_at) > now()){
+            if($sectionClassStudentTerm->academicSessionTerm->start_at > now()){
                 $term = $sectionClassStudentTerm->academicSessionTerm;
             }else if($sectionClassStudentTerm->academicSessionTerm->term_id == $this->currentSessionTerm()->term_id + 1){
                 $term = $sectionClassStudentTerm->academicSessionTerm;
